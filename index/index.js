182727d0830b14657a9f5a3c26a5b07bc95496f2
akbarjon <akbarjonsharifjonov33@gmail.com>
2024-09-26 23:17:02 +0500

    type

---
+++ index/index.js
@@ -0,0 +1,16 @@
+// let name = "John Doe";
+// let age = 30;
+// let isStudent = true;
+// let hobbies = ["Reading", "Gaming", "Coding"];
+
+// function greet(person: string): string {
+//   return `Hello, ${person}`;
+// }
+
+// function calculateSum(a: number, b: number): number {
+//   return a + b;
+// }
+
+// function isAdult(personAge: number): boolean {
+//   return personAge >= 18;
+// }
